f671c248d4fc8942d51e9e475baebde78a630d99
Kane Chang <kane.chang.fl@gmail.com>
2024-02-06 19:02:36 +0000

    added hover animation and fixed box-shadow bug

--- src/components/NavTabs.tsx
+++ src/components/NavTabs.tsx
@@ -22,13 +22,15 @@ const NavTabs = () => {
             to="/"
             end
             className={({ isActive }) =>
-              isActive ? "text-white active" : "text-accentOrange"
+              isActive
+                ? "text-white active "
+                : "text-accentOrange hover:text-accentOrange/70 transition"
             }
           >
             .kane-chang
           </NavLink>
           <a
-            className="hidden md:flex text-accentOrange"
+            className="hidden md:flex text-accentOrange hover:text-accentOrange/70 transition"
             href="https://drive.google.com/file/d/1tC2l8JGctKf1myJKMnhQVQFyUexpRQZn/view"
             target="blank"
           >
@@ -37,7 +39,7 @@ const NavTabs = () => {
         </div>
         <div className="">
           <button className="md:hidden navbar-toggler" onClick={toggleMenu}>
-            <i className="fa-solid fa-bars text-accentOrange hover:text-accentOrange/50"></i>
+            <i className="fa-solid fa-bars text-accentOrange hover:text-accentOrange/70 transition"></i>
           </button>
         </div>
         <div className="hidden md:flex space-x-5">
@@ -46,26 +48,20 @@ const NavTabs = () => {
             className={({ isActive }) =>
               isActive
                 ? "nav-link text-white active"
-                : "nav-link text-accentOrange"
+                : "nav-link text-accentOrange hover:text-accentOrange/70 transition"
             }
           >
             about()
           </NavLink>
-          <a
-            className=""
-            href="https://github.com/kane-chang"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fa-brands fa-github text-xl text-accentOrange"></i>
+          <a className="" href="https://github.com/kane-chang" target="_blank">
+            <i className="fa-brands fa-github text-xl text-accentOrange hover:text-accentOrange/70 transition"></i>
           </a>
           <a
             className=""
             href="https://www.linkedin.com/in/kane-chang/"
             target="_blank"
-            rel="noopener noreferrer"
           >
-            <i className="fa-brands fa-linkedin text-xl text-accentOrange"></i>
+            <i className="fa-brands fa-linkedin text-xl text-accentOrange hover:text-accentOrange/70 transition"></i>
           </a>
         </div>
       </nav>
@@ -78,12 +74,12 @@ const NavTabs = () => {
         <div className="flex px-10 py-10 text-xl text-inter font-bold items-center justify-between">
           <h2>Menu</h2>
           <button onClick={toggleMenu}>
-            <i className="fa-solid fa-xmark text-2xl"></i>
+            <i className="fa-solid fa-xmark text-2xl hover:text-accentOrange/70 transition"></i>
           </button>
         </div>
-        <div className="flex flex-col space-y-5 pl-10 mt-12">
+        <div className="flex flex-col space-y-5 pl-12 mt-12">
           <a
-            className="text-accentOrange"
+            className="text-accentOrange hover:text-accentOrange/70 transition"
             href="https://drive.google.com/file/d/1tC2l8JGctKf1myJKMnhQVQFyUexpRQZn/view"
             target="blank"
           >
@@ -94,28 +90,26 @@ const NavTabs = () => {
             className={({ isActive }) =>
               isActive
                 ? "nav-link text-white active"
-                : "nav-link text-accentOrange"
+                : "nav-link text-accentOrange hover:text-accentOrange/70 transition"
             }
             onClick={toggleMenu}
           >
             about()
           </NavLink>
           <a
-            className="text-accentOrange flex items-center"
+            className="text-accentOrange flex items-center hover:text-accentOrange/70 transition"
             href="https://github.com/kane-chang"
             target="_blank"
-            rel="noopener noreferrer"
           >
-            <i className="fa-brands fa-github text-xl text-accentOrange mr-3"></i>
+            <i className="fa-brands fa-github text-xl text-accentOrange "></i>
             GitHub
           </a>
           <a
-            className="text-accentOrange flex items-center"
+            className="text-accentOrange flex items-center hover:text-accentOrange/70 transition"
             href="https://www.linkedin.com/in/kane-chang/"
             target="_blank"
-            rel="noopener noreferrer"
           >
-            <i className="fa-brands fa-linkedin text-xl text-accentOrange mr-3"></i>
+            <i className="fa-brands fa-linkedin text-xl text-accentOrange"></i>
             LinkedIn
           </a>
         </div>
