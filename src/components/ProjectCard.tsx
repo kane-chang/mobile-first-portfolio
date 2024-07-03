bd3444b1480dee16e8aed5c20f4931c0a0fe2879
Kane Chang <kane.chang.fl@gmail.com>
2024-07-03 15:19:09 +0100

    added mobile responsive paragraph lengths and preview images logic

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -3,6 +3,7 @@ interface ProjectCardProps {
   cardClass: string;
   paragraph?: string;
   paragraph2?: string;
+  paragraphResponsive?: string;
   image?: string;
   alt?: string;
   list?: boolean;
@@ -12,28 +13,46 @@ interface ProjectCardProps {
 const ProjectCard = ({
   header,
   cardClass,
-  paragraph, paragraph2,
+  paragraph,
+  paragraph2,
+  paragraphResponsive,
   image,
   alt,
   list,
   listItems,
 }: ProjectCardProps) => {
   return (
-    <div className={cardClass + " flex flex-col bg-black rounded-xl border p-6 h-72 md:h-[22rem] xxl:h-[34.5rem] absolute w-full"}>
-      <h2 className="font-inter font-medium text-4xl xl:text-5xl pb-4">{header}</h2>
-      {paragraph ? <p className="font-inconsolata xl:text-xl">{paragraph}</p> : null}
-      {paragraph2 ? <p className="font-inconsolata xl:text-xl pt-2">{paragraph2}</p> : null}
+    <div
+      className={
+        cardClass +
+        " flex flex-col bg-black rounded-xl border p-6 h-72 md:h-[22rem] xxl:h-[34.5rem] absolute w-full"
+      }
+    >
+      <h2 className="font-inter font-medium text-4xl xl:text-5xl pb-4">
+        {header}
+      </h2>
+      {paragraph ? (
+        <p className="font-inconsolata xl:text-xl">{paragraph}</p>
+      ) : null}
+      {paragraphResponsive ? (
+        <p className="hidden lg:block font-inconsolata xl:text-xl pt-2">
+          {paragraphResponsive}
+        </p>
+      ) : null}
+      {paragraph2 ? (
+        <p className="font-inconsolata xl:text-xl pt-2">{paragraph2}</p>
+      ) : null}
       {image ? (
-        <img
-          className="object-scale-down w-full overflow-hidden"
-          src={image}
-          alt={alt}
-        />
+          <div className="object-scale-down w-10/12 overflow-hidden rounded-2xl border-white/20 border">
+            <img src={image} alt={alt} />
+          </div>
       ) : null}
       {list ? (
         <ul role="list" className="list-disc list-inside">
           {listItems?.map((item) => (
-            <li className="font-inconsolata xl:text-xl" key={item}>{item}</li>
+            <li className="font-inconsolata xl:text-xl" key={item}>
+              {item}
+            </li>
           ))}
         </ul>
       ) : null}
